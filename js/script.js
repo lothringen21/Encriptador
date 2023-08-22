fa1f8a1ab9be7a196a0fac15e13990ea9682df38
lothringen21 <anerolcherry@gmail.com>
2023-08-22 16:36:04 -0600

    Actualizando js

---
+++ js/script.js
@@ -0,0 +1,34 @@
+
+const sustituciones = [
+    ["a", "ai"],
+    ["e", "enter"],
+    ["i", "imes"],
+    ["o", "ober"],
+    ["u", "ufat"]
+];
+
+// Función para encriptar un mensaje
+function encriptarMensaje(texto) {
+    let mensajeEncriptado = texto;
+
+    for (const sustitucion of sustituciones) {
+        const letraOriginal = sustitucion[0];
+        const letraEncriptada = sustitucion[1];
+
+        const regex = new RegExp(letraOriginal, 'g');
+        mensajeEncriptado = mensajeEncriptado.replace(regex, letraEncriptada);
+    }
+
+    return mensajeEncriptado;
+}
+
+// Evento click para encriptar
+document.getElementById("encriptar").addEventListener("click", function () {
+    const textoOriginal = document.getElementById("cajaTexto").value;
+    const mensajeEncriptado = encriptarMensaje(textoOriginal);
+
+    document.getElementById("mensajeEncriptado").textContent = mensajeEncriptado;
+    
+});
+
+
